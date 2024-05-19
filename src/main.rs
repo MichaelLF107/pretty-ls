562e293bf2e3b1311fd10837739eacb6cd20c719
Michael Friedrich <michaelfriedrich253@gmail.com>
2024-05-19 12:36:08 -0300

    implementet directory listing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use std::fs;
+
+mod utils;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let mut path = if args.len() > 1 { &args[1] } else { "" };
+    let default_path = &String::from("./");
+
+    if path.is_empty() {
+        path = default_path;
+    }
+
+    let files = fs::read_dir(path).unwrap();
+
+    utils::print_files(files, path);
 }
